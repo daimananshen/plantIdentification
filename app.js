0772b08c4c8cc6119296a5b78395e69580944daa
gaolei <947458050@qq.com>
2020-06-10 15:44:49 +0800

    授权登录

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    var myCenter = wx.getStorageSync('myCenter') || []
+    myCenter.unshift(Date.now())
+    wx.setStorageSync('myCenter', myCenter)
 
     // 登录
     wx.login({
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,112 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
+var app = getApp();
+var that = '';
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    img: '/image/test_default.png',
+    imgB64: '',
+    content: '',
+    ishow: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    that = this;
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  /**
+   * 选择图片
+   */
+  chooseimgTap: function () {
+    that.setData({
+      ishow: false,
+      content: ''
+    });
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        const tempFilePaths = res.tempFilePaths[0];
+        that.getB64ByUrl(tempFilePaths);
+
+        that.setData({
+          img: tempFilePaths
+        });
+
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+  /**
+   * 转b64
+   */
+  getB64ByUrl: function (url) {
+    const FileSystemManager = wx.getFileSystemManager();
+    FileSystemManager.readFile({
+      filePath: url,
+      encoding: 'base64',
+      success(res) {
+        // console.log(res.data);
+        that.setData({
+          imgB64: res.data
+        });
+      }
+    })
+  },
+
+  /**
+   * 植物识别
+   */
+  plantTap: function (e) {
+    const imgB64 = that.data.imgB64;
+    if (!imgB64) {
+      that.setData({
+        ishow: true
+      });
+      return;
+    };
+
+    that.getToken(function (token) {
+      that.getResult(token);
+    });
+  },
+  getToken: function (callback) {
+    wx.request({
+      url: 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=OyxjHvcGqXlmD3skUUt3GHEl&client_secret=MUMttTySPycE2U9U25MqlCxdoQCpOwfa',
+      data: {},
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success(res) {
+        var token = res.data.access_token;
+        console.log(token);
+
+        return callback(token);
+      }
+    });
+  },
+  getResult: function (token) {
+    wx.request({
+      url: 'https://aip.baidubce.com/rest/2.0/image-classify/v1/plant?access_token=' + token, //仅为示例，并非真实的接口地址
+      method: "post",
+      data: {
+        image: that.data.imgB64
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success(res) {
+        console.log(res.data);
+        that.setData({
+          content: JSON.stringify(res.data)
+        });
+
+      }
+    });
   }
+
 })
