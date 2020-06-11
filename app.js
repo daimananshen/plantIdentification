a4e7bd172051c96549bc26c73e926c64769259d3
daimananshen <947458050@qq.com>
2020-06-11 21:51:45 +0800

    微信授权登录

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
+    // 云开发环境
     wx.cloud.init({
       env: 'dev-0a2xc'
     })
@@ -8,6 +9,6 @@ App({
   //全局变量
   globalData: {
     getData: '',
-    image:''
+    image: ''
   },
 })
