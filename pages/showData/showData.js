b004b79d00c46eda121c889ee55a314370cf1515
gaolei <947458050@qq.com>
2020-06-12 17:47:11 +0800

    首页上拉加载

--- pages/showData/showData.js
+++ pages/showData/showData.js
@@ -21,14 +21,14 @@ Page({
 
     for (let i = 0; i < dataList.length; i++) {
       if (dataList[i].baike_info) {
-        console.log("23", dataList[i])
+       
         this.content = dataList[i].baike_info
         this.dataName = dataList[i].name
         // 改变标题名称
         wx.setNavigationBarTitle({
           title: this.dataName
         })
-        console.log("26", this.content)
+        
       } else {
         this.noData = '抱歉!暂时没有找到具体的内容呢~QAQ'
       }
