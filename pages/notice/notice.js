daed428f2992bd9af5c94757c03b8c40463870b9
gaolei <947458050@qq.com>
2020-06-19 18:04:44 +0800

    发表文章

--- pages/notice/notice.js
+++ pages/notice/notice.js
@@ -1,30 +1,29 @@
 // pages/notice/notice.js
+var imgURL = ''
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    imgURL: ""
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
   //上传文件
   upload() {
     //把this赋值给that，就相当于that的作用域是全局的。
     let that = this;
-    console.log("jaj");
+
     wx.chooseImage({
       count: 1,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
       success(res) {
-        console.log("成功", res);
         that.uploadImage(res.tempFilePaths[0]);
       }
     })
@@ -35,7 +34,7 @@ Page({
       filePath: fileURL, // 小程序临时文件路径
       success: res => {
         // 返回文件 ID
-        console.log("上传成功", res)
+        imgURL = res.fileID
         //获取文件路径
         this.setData({
           imgURL: res.fileID
@@ -43,9 +42,35 @@ Page({
       },
       fail: console.error
     })
-  }
-  // formSubmit(e) {
-  //   console.log('form发生了submit事件，携带数据为：', e.detail.value)
-  // },
-  
+  },
+  formSubmit(e) {
+    //把数据给云数据库
+    const db = wx.cloud.database({});
+    const cont = db.collection('article');
+    cont.add({
+      data: {
+        title: e.detail.value.input,
+        content: e.detail.value.content,
+        image: imgURL,
+        updateTime: new Date()
+      },
+      success: function(res) {
+
+        wx.showModal({
+          title: '成功',
+          content: '您已经发布内容成功',
+          showCancel: false,
+          success(res) {
+            if (res.confirm) {
+              console.log(res.confirm)
+              wx.navigateTo({
+                url: '../index/index'
+              })
+            }
+          }
+        })
+      }
+    });
+  },
+
 })
