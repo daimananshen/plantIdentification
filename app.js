e5284acd44e46a17e68fba927393fff2894a1cc2
gaolei <947458050@qq.com>
2020-06-29 18:09:39 +0800

    登录缓存

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ App({
     // 云开发环境
     wx.cloud.init({
       env: 'dev-0a2xc'
-    })
-    
+    }) 
   },
   //全局变量
   globalData: {
--- pages/myCenter/myCenter.js
+++ pages/myCenter/myCenter.js
@@ -14,40 +14,32 @@ Page({
     })
   },
   onLoad: function() {
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
+    var that = this;
+    wx.getStorage({
+      key: 'getUserInfo',
+      success: function(res) {
+        console.log(res)
+        that.setData({
+          userInfo: res.data,
           hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+        });
+      },
+      fail: function(res) {}
+    });
   },
   getUserInfo: function(e) {
     if (e.detail.userInfo) {
       app.globalData.userInfo = e.detail.userInfo
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
+      var that = this;
+      wx.setStorage({
+        key: "getUserInfo",
+        data: app.globalData.userInfo,
+        success: function(res) {
+          that.setData({
+            userInfo: app.globalData.userInfo,
+            hasUserInfo: true
+          });
+        }
       })
     } else {
       //用户按了拒绝按钮
